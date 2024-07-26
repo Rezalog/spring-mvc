b25e6bac6ed1157d872b74e2bb6d8181fdd0a47c
Rezal <rladlfdnd369@naver.com>
2024-07-26 14:22:49 +0900

    [3] MVC FW - v2: MyView 객체로 View 분리

---
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        request.setAttribute("member", member);
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
+}
